021845d60cf6c7a1e67a41e93df7abb10e3824ba
Anthony Petrov <beavean@gmail.com>
2022-06-02 13:55:02 +0300

    Initial Commit

---
+++ BillSplit/BillSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitApp.swift
+//  BillSplit
+//
+//  Created by Beavean on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
